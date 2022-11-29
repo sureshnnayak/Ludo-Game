2537594ab59fb132fb3b76c78558c583af3c4a03
Suresh Nayak <sureshnnayak24@gmail.com>
2022-11-28 23:28:57 -0700

    working code

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     var human:Human = Human(id: "P1", name: "Player1")
     
+    @IBOutlet weak var dice: UIImageView!
     @IBOutlet weak var boardView: BoardView!
     
     let diceImage = ["one", "two", "three", "four", "five", "six"]
@@ -24,7 +25,7 @@ class ViewController: UIViewController {
         dice.image = UIImage(named: "five")
     }
 
-     @IBOutlet weak var dice: UIImageView!
+  
     
     @IBAction func rollDice(_ sender: Any) {
         dice.image = UIImage(named:diceImage[Int(arc4random_uniform(6))] )
